46ef71029a8cb0da2ced7c785e4fd0649dfe61ea
Vikrinox <51887867+Vickerinox@users.noreply.github.com>
2024-11-07 09:32:10 +0100

    changed arm7 gathering method

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,36 @@
 /// This function steals control of the ARM7 CPU assuming it is running in the sync loop within the bootloader.
 /// The way it does this is by stealing some unused WRAM, writing code it, and mapping it to the memory where
 /// the ARM7 is executing (the small sync loop)
-/// 
-/// TODO: instead of using NOP instructions that leads the CPU down the bank until it hits code use a jump table.
 pub unsafe fn steal_arm7() {
 
-    //steal WRAM-C4 from the arm7, as it *should* be unused while it's sync-looping.
-    core::ptr::write_volatile(0x4004050 as *mut u8, 0b10000000);
+    //offsets in words (4 bytes) into the WRAM were about to steal
+    //JT here means "jump table", offsets could likely be tweaked, but i've not bothered to tune them.
+    const ENTRYPOINT_OFFSET: usize = 0x800;
+    const JT_START: usize = 0x400;
+    const JT_END: usize = ENTRYPOINT_OFFSET-1; //-1 here prevents some shenanigans, don't worry about it.
+    const BRANCH_BASE: usize = ENTRYPOINT_OFFSET-2; //because ARM instructions are "special", this is correct.
 
-    //map the WRAM to our cpu, making it visible in 0x0300_0000
+    //some magic constants never hurt ;)
+    const BLANK_BRANCH_INSTRUCTION: u32 = 0xEA000000;
+    const STOLEN_WRAM: *mut u32 = 0x03000000 as *mut u32;
+    
+    //steal WRAM-C4 from the arm7, as it *should* be unused while it's working.
+    core::ptr::write_volatile(0x4004050 as *mut u8, 0b10000000);
+    //map the WRAM to our cpu (arm7), and make it visible in 0x0300_0000
     core::ptr::write_volatile(0x400405C as *mut u32, 0x100000);
 
-    //Write NOP instructions to the area which the arm7 will be occupying (WRAM C slot 7)
-    for i in 0x400..0x800 {
-        core::ptr::write_volatile((0x3000000+(i<<2)) as *mut u32, 0xE1A00000);
+    //Write a big series of branch instructions that makes the arm7 jump directly to our entrypoint later
+    for i in JT_START..JT_END {
+        core::ptr::write_volatile(STOLEN_WRAM.add(i), BLANK_BRANCH_INSTRUCTION | (BRANCH_BASE-i) as u32);
     }
-    //Write our binary (for now a branch instruction that jumps to itself, AKA infinite loop.)
-    core::ptr::write_volatile((0x3000000+(0x800<<2)) as *mut u32, 0xEAFFFFFE);
+    //Write our entrypoint (for now a branch instruction that jumps to itself, AKA infinite loop.)
+    core::ptr::write_volatile(STOLEN_WRAM.add(ENTRYPOINT_OFFSET), 0xEAFFFFFE);
    
     //overwrite the WRAM bank the arm7 is currently executing in with ours
     core::ptr::write_volatile((0x4004050) as *mut u8, 0b10011101); //enable our hijacked one
     core::ptr::write_volatile((0x4004053) as *mut u8, 0);           //disable the old one (unneccesary maybe?)
     
+    //congrats! Now the arm7 is stolen.
 }
 
 
@@ -43,6 +52,7 @@ pub fn _start() {
 }
 
 //Really our code should NEVER panic, but we still need this.
+#[cfg(not(test))] //works to shut up rust-analyzer in vscode. It keeps thinking we still have std...
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
