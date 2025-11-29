1809d31fe92ac98a2a73a20b0d3dc51740aca869
Vickerinox <05viktorinox@gmail.com>
2025-11-29 09:56:23 +0100

    Replace inline asm jump with function pointer call
    Replaces the use of inline assembly for jumping to an address with a direct function pointer call. Adds VCOUNT register polling before the jump for synchronization. The previous inline assembly is now commented out.

--- arm9_bin/src/main.rs
+++ arm9_bin/src/main.rs
@@ -409,9 +409,14 @@ unsafe fn main() {
                         reboot_lib::disable_all_interrupts();
                         flush_mmc();
                         flush_mmc();
+                        const VCOUNT_REG: *const u16 = 0x4000006 as *const u16;
+                        while VCOUNT_REG.read_volatile() != 192 {}
+                        while VCOUNT_REG.read_volatile() == 192 {}
                         arm9_send_arm7_jump(0x2FDF000);
-                        #[cfg(target_arch="arm")]
-                        core::arch::asm!("bx r0", in("r0") 0x2FD8000);
+                        (*(0x2FD8000 as *mut unsafe extern "C" fn()))();
+                        
+                        //#[cfg(target_arch="arm")]
+                        //core::arch::asm!("bx r0", in("r0") );
                         loop {}
                     }
                     if let Some(loading_mod) = loading_mod_file.take() {
