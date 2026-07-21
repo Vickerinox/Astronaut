25580c87ec15ece359a2e06864857f46b68acfc1
vikrinox <viktor@koda.re>
2026-07-21 16:47:01 +0200

    add assert messages to arm9 resources and make the conditions more reasonable

--- astronaut/arm9/src/resources/mod.rs
+++ astronaut/arm9/src/resources/mod.rs
@@ -19,10 +19,12 @@ macro_rules! include_bytes_word_align {
 
 pub const FONT_FILE: &[u8] =
     include_bytes_word_align!(concat!(env!("OUT_DIR"), "/font_compressed.bin"));
-reboot_lib::const_assert!(FONT_FILE.len() < 0x800);
+reboot_lib::const_assert!(FONT_FILE.len() < 0x800, "Failed to compress Font!");
+reboot_lib::const_assert!(FONT_FILE.len() > 0, "Please build the default Font before building the ARM9 Binary");
+
 
 pub const ARM7_BINARY: &[u8] = include_bytes_word_align!(concat!(env!("OUT_DIR"), "/arm7.bin"));
-reboot_lib::const_assert!(ARM7_BINARY.len() < 0x4000);
+reboot_lib::const_assert!(ARM7_BINARY.len() > 0, "Please build the ARM7 binary before building the ARM9 Binary");
 
 pub const BOOTSTRAP_BINARY: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/bootstrap.bin"));
-reboot_lib::const_assert!(BOOTSTRAP_BINARY.len() < 0x4000);
+reboot_lib::const_assert!(BOOTSTRAP_BINARY.len() > 0, "Please build the Bootstrap binary before building the ARM9 Binary");
